37897284b4497e3c3ede520eddf3872d77181752
chestozo <chestozo@yandex-team.ru>
2013-05-26 22:23:33 +0400

    move main formatter code to func

--- code-beautify.py
+++ code-beautify.py
@@ -1,21 +1,22 @@
 import sublime, sublime_plugin, subprocess
 
-# via http://www.bergspot.com/blog/2012/05/formatting-xml-in-sublime-text-2-xmllint/
-class CodeBeautifyXmlCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-      command = "XMLLINT_INDENT=$'\t' xmllint --format --encode utf-8 -"
+def beautify_code(self, edit, command):
+    # help from http://www.sublimetext.com/forum/viewtopic.php?f=2&p=12451
+    xmlRegion = sublime.Region(0, self.view.size())
+    p = subprocess.Popen(command, bufsize=-1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
+    result, err = p.communicate(self.view.substr(self.view.sel()[0]).encode('utf-8'))
 
-      # help from http://www.sublimetext.com/forum/viewtopic.php?f=2&p=12451
-      xmlRegion = sublime.Region(0, self.view.size())
-      p = subprocess.Popen(command, bufsize=-1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
-      result, err = p.communicate(self.view.substr(self.view.sel()[0]).encode('utf-8'))
-
-      if err != "":
+    if err != "":
         self.view.set_status('xmllint', "xmllint: "+err)
         sublime.set_timeout(self.clear,10000)
-      else:
+    else:
         self.view.replace(edit, self.view.sel()[0], result.decode('utf-8'))
         sublime.set_timeout(self.clear,0)
 
+# via http://www.bergspot.com/blog/2012/05/formatting-xml-in-sublime-text-2-xmllint/
+class CodeBeautifyXmlCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        beautify_code(self, edit, "XMLLINT_INDENT=$'\t' xmllint --format --encode utf-8 -")
+
     def clear(self):
-      self.view.erase_status('xmllint')
+        self.view.erase_status('xmllint')
