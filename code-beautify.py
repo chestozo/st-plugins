e1812f77ae585f930df3ed02de1b432e9eff6cf3
chestozo <chestozo@yandex-team.ru>
2013-06-09 13:37:33 +0400

    now yes: st2 was crashing when I added quotes to file-name...

--- code-beautify.py
+++ code-beautify.py
@@ -1,22 +1,27 @@
+# Based on: http://www.bergspot.com/blog/2012/05/formatting-xml-in-sublime-text-2-xmllint/
+# use shell in python: http://www.sublimetext.com/forum/viewtopic.php?f=2&p=12451
+# shell quote: http://stackoverflow.com/a/35857/449345
 import sublime, sublime_plugin, subprocess
 
-def beautify_code(self, edit, command):
-    # help from http://www.sublimetext.com/forum/viewtopic.php?f=2&p=12451
+def shellquote(s):
+    return "'" + s.replace("'", "'\\''") + "'"
+
+def beautify_code(self, edit, command, command_name):
     xmlRegion = sublime.Region(0, self.view.size())
     p = subprocess.Popen(command, bufsize=-1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
     result, err = p.communicate(self.view.substr(self.view.sel()[0]).encode('utf-8'))
 
     if err != "":
-        self.view.set_status('xmllint', "xmllint: "+err)
+        self.view.set_status(command_name, command_name + ": " + err)
         sublime.set_timeout(self.clear,10000)
     else:
         self.view.replace(edit, self.view.sel()[0], result.decode('utf-8'))
         sublime.set_timeout(self.clear,0)
 
-# via http://www.bergspot.com/blog/2012/05/formatting-xml-in-sublime-text-2-xmllint/
+
 class CodeBeautifyXmlCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        beautify_code(self, edit, "XMLLINT_INDENT=$'\t' xmllint --format --encode utf-8 -")
+        beautify_code(self, edit, "XMLLINT_INDENT=$'\t' xmllint --format --encode utf-8 -", 'xmllint')
 
     def clear(self):
         self.view.erase_status('xmllint')
@@ -24,18 +29,18 @@ class CodeBeautifyXmlCommand(sublime_plugin.TextCommand):
 class CodeBeautifyHtmlCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.run_command('select_all')
-        beautify_code(self, edit, "~/local/bin/node ~/configs/tools/tabifier.js '" + self.view.file_name() + "'")
+        beautify_code(self, edit, "~/local/bin/node ~/configs/tools/tabifier.js " + shellquote(self.view.file_name()), 'tabifier')
 
     def clear(self):
-        self.view.erase_status('xmllint')
+        self.view.erase_status('tabifier')
 
 class CodeBeautifyJsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.run_command('select_all')
-        beautify_code(self, edit, "python ~/configs/tools/jsbeautifier.py -d '" + self.view.file_name() + "'")
+        beautify_code(self, edit, "python ~/configs/tools/jsbeautifier.py -d " + shellquote(self.view.file_name()), 'jsbeautifier')
 
     def clear(self):
-        self.view.erase_status('xmllint')
+        self.view.erase_status('jsbeautifier')
 
 
 
