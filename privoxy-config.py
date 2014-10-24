c33083cc8c18a3d84f39a2f54655e6e4e49d95bc
chestozo <chestozo@yandex-team.ru>
2014-10-24 18:11:26 +0400

    set syntax async

--- privoxy-config.py
+++ privoxy-config.py
@@ -3,5 +3,13 @@ import sublime, sublime_plugin
 # Opens privoxy config file and sets syntax to makefile
 class PrivoxyConfigCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.window().open_file('~/configs/privoxy/my.action')
-        self.view.set_syntax_file('Packages/Makefile/Makefile.tmLanguage')
+        view = self.view.window().open_file('~/configs/privoxy/my.action')
+
+        # open_file is a async function so that we need to wait view to load
+        def set_syntax():
+            if view.is_loading():
+                sublime.set_timeout(set_syntax, 100)
+            else:
+                view.set_syntax_file('Packages/Makefile/Makefile.tmLanguage')
+
+        set_syntax()
