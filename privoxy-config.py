8fa0072de0827a81d5209cc95fb472255795a9f6
Roman Kartsev <chestozo@yandex-team.ru>
2016-10-04 01:41:37 +0300

    privoxy main config file edit command

--- privoxy-config.py
+++ privoxy-config.py
@@ -14,6 +14,20 @@ class PrivoxyConfigCommand(sublime_plugin.WindowCommand):
 
         set_syntax()
 
+# Opens privoxy main config file and sets syntax to makefile
+class PrivoxyMainConfigCommand(sublime_plugin.WindowCommand):
+    def run(self, paths = [], name = ""):
+        view = self.window.open_file('/usr/local/etc/privoxy/config')
+
+        # open_file is a async function so that we need to wait view to load
+        def set_syntax():
+            if view.is_loading():
+                sublime.set_timeout(set_syntax, 100)
+            else:
+                view.set_syntax_file('Packages/Makefile/Makefile.tmLanguage')
+
+        set_syntax()
+
 class YaxyConfigCommand(sublime_plugin.WindowCommand):
     def run(self, paths = [], name = ""):
         view = self.window.open_file('~/configs/.yaxyrc')
